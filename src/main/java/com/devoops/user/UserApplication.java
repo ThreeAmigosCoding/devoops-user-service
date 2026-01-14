fd45fff6f108a7bbb269b5d1503ca6fe08bd5367
Dusan <dusan_rozic@hotmail.co.uk>
2026-01-14 19:57:00 +0100

    add test endpoint

--- src/main/java/com/devoops/user/UserApplication.java
+++ src/main/java/com/devoops/user/UserApplication.java
@@ -2,8 +2,9 @@ package com.devoops.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class UserApplication {
 
 	public static void main(String[] args) {
