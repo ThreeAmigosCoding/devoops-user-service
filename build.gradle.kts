a09307cd872592c9430cf8d0ba73a19a6e340cbe
Marko Janošević <markojanosevicsm7@gmail.com>
2026-01-17 23:18:46 +0100

    feat: Integrate Logstash logging with Logback configuration
    Added logstash-logback-encoder dependency and configured Logback for structured logging to Logstash. Enhanced TestController with MDC and logging statements. Updated application properties for logging settings and added Logback configuration files for main and test environments.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 //	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.flywaydb:flyway-database-postgresql")
+	implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
 //	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
