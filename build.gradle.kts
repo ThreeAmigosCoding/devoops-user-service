2ff4eb91d3a56bed9bb716c152b4471e74aef666
Dusan <dusan_rozic@hotmail.co.uk>
2026-02-02 20:52:19 +0100

    fix: exclude DataSourceAutoConfiguration in UserApplicationTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 
 	implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 	runtimeOnly("org.postgresql:postgresql")
-//	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
 //	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
